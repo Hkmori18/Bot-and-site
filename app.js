0f8bfaa8d63d3af382dd2ec2077ce028103a25f9
Hkmori15 <animeshnik27472@gmail.com>
2023-08-11 16:53:54 +0200

    Переписан полностью код

--- app.js
+++ app.js
@@ -1,88 +1,37 @@
-require('dotenv').config()
-
 const TelegramBot = require('node-telegram-bot-api')
 const axios = require('axios')
-const TELEGRRAM_BOT_TOKEN = '6360097777:AAH4cTmFmYxl-h6To2KXJ8jH8hC-x7cnLGo'
-const OPENWEATHERMAP_API_KEY = '9763217e6602f16a5c39c79d7aa2af4f'
-const bot = new TelegramBot(TELEGRRAM_BOT_TOKEN, { polling: true })
-const storage = {}
+const token = '6360097777:AAH4cTmFmYxl-h6To2KXJ8jH8hC-x7cnLGo'
+
+const bot = new TelegramBot(token, { polling: true })
 
 bot.onText(/\/start/, msg => {
 	const chatId = msg.chat.id
 	bot.sendMessage(
-		chatId,
-		'Привет, этот бот может показывать погоду для любого города.Для этого, используй параметры ниже:',
-		{
-			reply_markup: {
-				inline_keyboard: [
-					[{ text: 'Узнать погоду', callback_data: 'get_weather' }],
-				],
-			},
-		}
+		'Привет, этот бот может показывать погоду для любого города. Для этого напиши название города'
 	)
 })
-
-bot.on('callback_query', async callbackQuery => {
-	const chatId = callbackQuery.message.chat.id
-	const data = callbackQuery.data
-
-	switch (data) {
-		case 'get_weather':
-			const userDataWeather = getUserData(chatId)
-			userDataWeather.waitingForCity = true
-			userDataWeather.waitingForWeather = true
-			bot.sendMessage(
-				chatId,
-				'Пожалуйста, введите имя города или отправьте /stop чтобы остановить:'
-			)
-			break
-
-		default:
-			break
-	}
-})
-
-function getUserData(chatId) {
-	let userData = storage[chatId]
-	if (!userData) {
-		userData = {
-			waitingForCity: false,
-			waitingForWeather: false,
-		}
-		storage[chatId] = userData
-	}
-	return userData
-}
 
 bot.on('message', async msg => {
 	const chatId = msg.chat.id
-	const text = msg.text
+	const userInput = msg.text
 
-	const userData = getUserData(chatId)
-	if (userData && userData.waitingForCity) {
-		const city = text
-		let messageText = ''
-		if (userData.waitingForWeather) {
-			messageText = await getWeatherData(city)
-		}
-		bot.sendMessage(chatId, messageText)
-		resetUserData(chatId)
+	try {
+		const response = await axios.get(
+			`https://api.openweathermap.org/data/2.5/weather?q=${userInput}&lang=ru&appid=68dad6d6ab6d64087dd084d5e57b69b0`
+		)
+		const data = response.data
+		const weather = data.weather[0].description
+		const temperature = data.main.temp - 273.15
+		const city = data.name
+		const humidity = data.main.humidity
+		const pressure = data.main.pressure
+		const windSpeed = data.wind.speed
+		const message = `Погода в ${city} - ${weather} температура ${temperature.toFixed(
+			2
+		)}°C. Влажность - ${humidity}%, давление - ${pressure} Па, и скорость ветра - ${windSpeed}м/с.`
+
+		bot.sendMessage(chatId, message)
+	} catch (error) {
+		bot.sendMessage(chatId, 'Иди глянь в окошко ;)')
 	}
 })
-
-function resetUserData(chatId) {
-	const userData = getUserData(chatId)
-	userData.waitingForCity = false
-	userData.waitingForWeather = false
-}
-
-async function getWeatherData(city) {
-	const response = await axios.get(
-		`http://api.openweathermap.org/data/2.5/weather?q=${city}&lang=ru&appid=${OPENWEATHERMAP_API_KEY}`
-	)
-	const weatherData = response.data
-	const weatherDescription = weatherData.weather[0].description
-	const temperature = Math.round(weatherData.main.temp - 273.15)
-	const messageText = `Погода в городе ${city} это ${weatherDescription} с температурой ${temperature} °C.`
-	return messageText
-}
